a039bbba2958d48dcff5515fd43e0c1fca3615ce
joannah2 <joannah2@illinois.edu>
2021-04-19 12:57:01 -0500

    Simplify game level to represent one level instance

--- include/core/game_level.h
+++ include/core/game_level.h
@@ -7,24 +7,19 @@
 namespace bubblebounce {
 
 /**
- * The class representing each level's starting state.
+ * The class representing each level.
  */
   class GameLevel {
   public:
-    GameLevel(const glm::vec2& left_corner, const glm::vec2& right_corner);
+    GameLevel(const size_t& level_number, const std::vector<Bubble>& bubbles);
     
-    //generate the positions for the bubbles at the game level 
-    void GenerateBubblePositions(std::vector<Bubble>& bubbles);
-
     size_t GetLevel() const;
-    size_t GetInitialBubbleCount() const;
+    std::vector<Bubble> GetLevelBubbles() const;
   private:
     size_t level_number_;
-    size_t initial_bubble_count;
-    glm::vec2 left_corner_;
-    glm::vec2 right_corner_;
-    static constexpr int kBubbleMultiplier = 20;
-    static constexpr size_t kNumberOfRows = 3;
+    size_t player_lives_;
+    std::vector<Bubble> bubbles_;
+
   };
 
 }  // namespace bubblebounce
--- src/core/game_level.cc
+++ src/core/game_level.cc
@@ -3,37 +3,17 @@
 namespace bubblebounce {
 
 
-  GameLevel::GameLevel(const glm::vec2& left_corner, const glm::vec2& right_corner) {
-    level_number_ = 1;
-    left_corner_ = left_corner;
-    right_corner_ = right_corner;
-    initial_bubble_count = level_number_ * kBubbleMultiplier;
+  GameLevel::GameLevel(const size_t& level_number, const std::vector<Bubble>& bubbles) {
+    level_number_ = level_number;
+    bubbles_ = bubbles;
   }
 
   size_t GameLevel::GetLevel() const {
     return level_number_;
   }
 
-  void GameLevel::GenerateBubblePositions(std::vector<Bubble>& bubbles) {
-    size_t distance = (size_t) bubbles.at(0).GetRadius() * 2;
-    size_t x_1 = (size_t) left_corner_.x + distance;
-    size_t y_1 = (size_t) left_corner_.y + distance;
-    size_t bubble_index = 0;
-    
-    for (size_t y = y_1; y < right_corner_.y - distance; y += distance) {
-      if (bubble_index >= initial_bubble_count) break;
-      for (size_t x = x_1; x < right_corner_.x - distance; x += distance) {
-        glm::vec2 position = glm::vec2{x, y};
-        bubbles.at(bubble_index).SetPosition(position);
-        ++bubble_index;
-        if (bubble_index >= initial_bubble_count) break;
-      }
-    }
+  std::vector<Bubble> GameLevel::GetLevelBubbles() const {
+    return bubbles_;
   }
-
-  size_t GameLevel::GetInitialBubbleCount() const {
-    return initial_bubble_count;
-  }
-
 
 }  // namespace bubblebounce
