550c7b25611bf4172718b5fa6801d7b5058db138
joannah2 <joannah2@illinois.edu>
2021-04-23 15:18:37 -0500

    Store Game Levels within Generator

--- include/core/level_generator.h
+++ include/core/level_generator.h
@@ -11,53 +11,67 @@ namespace bubblebounce {
  */
   class LevelGenerator {
   public:
+    /**
+     * Initializes all game levels with their respective level number, lives, 
+     * and bubbles.
+     * @param left_corner vec2 coordinates of the game's top left window
+     * @param right_corner vec2 coordinates of the game's bottom right window
+     */
     LevelGenerator(const glm::vec2& left_corner,
                    const glm::vec2& right_corner);
-    /**
-     * Create vector of bubbles for each level to be called by bubble_bounce_app
-     * when initializing the 
-     * @return 
-     */
-    std::vector<std::vector<Bubble>> GetGameLevelBubbles();
-
-    /**
- * Creates the preset positions for the bubbles for a given level.
- * @param bubbles bubbles to generate positions for
- * @param level the corresponding level
- */
-    void GenerateBubblePositions(std::vector<Bubble>& bubbles,
-                                 const size_t& level);
-
-    // TODO:: remove 2d vector getter & storing a copy of all bubbles vectors for each level
-    // bubble_bounce_app will call GetGameLevelBubbles(const size_t& level) to get the bubbles for that particular level
-    std::vector<Bubble> GetGameLevelBubbles(const size_t& level);
     
-    std::vector<Bubble> GenerateBubblePositionsForLevel(const size_t& level,
-                                                  std::vector<Bubble>& bubbles);
+    /**
+     * Returns the GameLevel object for a given level.
+     * @param level the level of the desired GameLevel
+     * @return generated GameLevel object for the level 
+     */
+    GameLevel GetGeneratedGameLevel(const size_t& level) const;
+    
+    /**
+     * Returns the default bubble count for the given level.
+     * @param level level number 
+     * @return level size for a given level
+     */
     size_t GetLevelDefaultBubbleCount(const size_t& level) const;
     
-    // create random starting velocity for the ball
-    glm::vec2 GenerateBallStartingVelocity() const;
-    
+    /**
+     * Gets the number of levels.
+     * @return number of levels
+     */
     static size_t GetNumberOfLevels();
     
     
-    
   private:
-//    std::vector<Bubble> bubbles_;
-    glm::vec2 left_corner_;
-    glm::vec2 right_corner_;
+    // level "0" considered a level
     static constexpr size_t kNumberOfLevels = 2;
+    
+    // vector index is the level to which the value corresponds to
+    // position 0 corresponds to an empty level
+    // (ie simplifies indexing a vector_of_bubbles[level] instead of 
+    // vector_of_bubbles[level - 1])
     const std::vector<size_t> kInitialBubbleCount{0, 35};
     const std::vector<size_t> kInitialUnpoppableBubbles{0, 4};
     const std::vector<size_t> kInitialSpecialBubbles{0, 5};
+    const std::vector<size_t> kInitialStartingLives{0, 5};
     
-//    static constexpr int kBubbleMultiplier = 20;
     static constexpr float kBubbleRadius = 40.0f;
     
-    // setters to generate bubble positions for each level
-    void SetLevel1Bubbles(std::vector<Bubble>& bubbles);
-    void SetLevel2Bubbles(std::vector<Bubble>& bubbles);
+    glm::vec2 left_corner_;
+    glm::vec2 right_corner_;
+    std::vector<GameLevel> levels_;
+    
+    /**
+     * Initializes game levels with respective randomized bubbles.
+     */
+    void GenerateGameLevels();
+
+    /**
+    * Creates the preset positions for the bubbles for a given level.
+    * @param bubbles bubbles to generate positions for
+    * @param level the corresponding level
+    */
+    void GenerateBubblePositions(std::vector<Bubble>& bubbles,
+                                 const size_t& level);
   };
 
 }  // namespace bubblebounce
--- src/core/level_generator.cc
+++ src/core/level_generator.cc
@@ -5,25 +5,40 @@ namespace bubblebounce {
 
   LevelGenerator::LevelGenerator(const glm::vec2& left_corner,
                                  const glm::vec2& right_corner) {
+    // check initial values match
+    if (kNumberOfLevels != kInitialBubbleCount.size() 
+        || kNumberOfLevels != kInitialUnpoppableBubbles.size()
+           || kNumberOfLevels != kInitialSpecialBubbles.size()
+              || kNumberOfLevels != kInitialStartingLives.size()) {
+      throw std::invalid_argument("Initial values don't match number"
+                                  "of levels");
+    }
+    
+    // check corner coordinates
+    if (left_corner.x >= right_corner.x  || left_corner.y >= right_corner.y) {
+      throw std::invalid_argument("Top left corner coordinates must be "
+                                  "less than bottom right corner coordinates.");
+    }
+    
     left_corner_ = left_corner;
     right_corner_ = right_corner;
+    GenerateGameLevels();
   }
-
-  // return 2d vector of bubbles to be called in bubblebounce app to set in the container
-  std::vector<std::vector<Bubble>> LevelGenerator::GetGameLevelBubbles() {
-    std::vector<std::vector<Bubble>> level_bubble_defaults;
+  
+  void LevelGenerator::GenerateGameLevels() {
     std::vector<Bubble> bubbles;
+    
     ci::Color color;
     Bubble::BubbleType type;
     Bubble::BubbleState state;
-    int rand;
     size_t unpoppable_count = 0;
     size_t special_count = 0;
     
+    int rand;
     std::random_device random_device;
     std::mt19937 random_engine(random_device());
     std::uniform_int_distribution<int> int_distribution(0, 3);
-    
+
     for (size_t level = 0; level < kInitialBubbleCount.size(); ++level) {
       for (size_t i = 0; i < kInitialBubbleCount[level]; ++i) {
         rand = int_distribution(random_engine);
@@ -47,12 +62,11 @@ namespace bubblebounce {
         bubbles.emplace_back(bubble);
       }
       GenerateBubblePositions(bubbles, level);
-      level_bubble_defaults.emplace_back(bubbles);
+      levels_.emplace_back(GameLevel(level,
+                           kInitialStartingLives[level], bubbles));
     }
-    return level_bubble_defaults;
   }
   
-  // Temporary bubble position generator for a simple level
   void LevelGenerator::GenerateBubblePositions(std::vector<Bubble>& bubbles,
                                                const size_t& level) {
     size_t distance = (size_t) kBubbleRadius * 2;
@@ -70,58 +84,22 @@ namespace bubblebounce {
       }
     }
   }
-  // When there are more levels:
-  // Have each setter for the different levels return the bubbles (not void)
-//  std::vector<Bubble> LevelGenerator::GenerateBubblePositionsForLevel(const size_t& level, std::vector<Bubble>& bubbles) {
-//    // check the given bubbles match the size
-//    
-//    switch (level) {
-//      case 1:
-//        SetLevel1Bubbles(bubbles);
-//        break;
-//      case 2:
-//        SetLevel2Bubbles(bubbles);
-//        break;
-//    }
-//    
-//    return bubbles_;
-//  }
-//
-//  void LevelGenerator::SetLevel1Bubbles(std::vector<Bubble>& bubbles) {
-//    size_t distance = (size_t) bubbles_.at(0).GetRadius() * 2;
-//    size_t x_1 = (size_t) left_corner_.x + distance;
-//    size_t y_1 = (size_t) left_corner_.y + distance;
-//    size_t bubble_index = 0;
-//
-//    for (size_t y = y_1; y < right_corner_.y - distance; y += distance) {
-//      if (bubble_index >= initial_bubble_count) break;
-//      for (size_t x = x_1; x < right_corner_.x - distance; x += distance) {
-//        glm::vec2 position = glm::vec2{x, y};
-//        bubbles.at(bubble_index).SetPosition(position);
-//        ++bubble_index;
-//        if (bubble_index >= initial_bubble_count) break;
-//      }
-//    }
-//  }
   
-  void LevelGenerator::SetLevel2Bubbles(std::vector<Bubble> &bubbles) {
-
+  GameLevel LevelGenerator::GetGeneratedGameLevel(const size_t& level) const {
+    if (level >= kNumberOfLevels) {
+      throw std::invalid_argument("Requested level does not exist");
+    }
+    return levels_[level];
   }
   
-  size_t LevelGenerator::GetLevelDefaultBubbleCount(const size_t& level) const {
+  size_t LevelGenerator::GetLevelDefaultBubbleCount(const size_t& level) const {\
+    if (level >= kNumberOfLevels) {
+      throw std::invalid_argument("Requested level does not exist");
+    }
     return kInitialBubbleCount[level];
   }
-  
+
   size_t LevelGenerator::GetNumberOfLevels() {
     return kNumberOfLevels;
   }
-
-  glm::vec2 LevelGenerator::GenerateBallStartingVelocity() const {
-    std::random_device random_device;
-    std::mt19937 random_engine(random_device());
-    std::uniform_real_distribution<float> distribution(9.99f, 10.01f);
-    glm::vec2 velocity{distribution(random_engine), -1 * distribution(random_engine)};
-    return velocity;
-  }
-
 }  // namespace bubblebounce
