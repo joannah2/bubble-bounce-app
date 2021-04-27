7699263f57426b068eb01efa48eafb548eb38314
joannah2 <joannah2@illinois.edu>
2021-04-27 17:43:04 -0500

    Add == operator and logic tests

--- tests/test_game_level.cc
+++ tests/test_game_level.cc
@@ -1,5 +1,4 @@
 #include <core/game_level.h>
-#include <core/bubble.h>
 
 #include <catch2/catch.hpp>
 
@@ -10,6 +9,7 @@ using bubblebounce::Bubble;
  * Test Strategy for Game Level
  * - initializing attributes
  *  - getters/setters for bubbles vector
+ *  - handling level winning/losing/resetting logic
  */
 TEST_CASE("Test constructor and retrieving level attributes") {
   SECTION("Empty constructor proper initialization") {
@@ -17,12 +17,6 @@ TEST_CASE("Test constructor and retrieving level attributes") {
     REQUIRE(level.GetLevel() == 0);
     REQUIRE(level.GetPlayerLives() == 0);
     REQUIRE(level.GetLevelBubbles().empty());
-  }
-
-  SECTION("GameLevel invalid starting lives") {
-    std::vector<Bubble> bubbles;
-    REQUIRE_THROWS_AS(GameLevel(1, 0, bubbles), 
-                      std::invalid_argument);
   }
 
   SECTION("GameLevel proper initialization") {
@@ -66,5 +60,79 @@ TEST_CASE("Test Getters/Setters for bubbles") {
     for (size_t i = 0; i < red_bubbles.size(); ++i) {
       REQUIRE(red_bubbles[i] == level_bubbles[i]);
     }
+  }
+}
+
+TEST_CASE("Test comparison operator for levels") {
+  std::vector<Bubble> white_bubbles;
+  for (size_t i = 0; i < 5; ++i) {
+    Bubble bubble("white", glm::vec2{0,0}, 5.0f,
+                  bubblebounce::Bubble::NormalBubble,
+                  bubblebounce::Bubble::Strong);
+    white_bubbles.emplace_back(bubble);
+  }
+
+  std::vector<Bubble> red_bubbles;
+  for (size_t i = 0; i < 5; ++i) {
+    Bubble bubble("red", glm::vec2{0,0}, 10.0f,
+                  bubblebounce::Bubble::SpecialBubble,
+                  bubblebounce::Bubble::Medium);
+    red_bubbles.emplace_back(bubble);
+  }
+
+  GameLevel white_level(1, 5, white_bubbles);
+  GameLevel identical_white_level(1, 5, white_bubbles);
+  GameLevel red_level(1, 5, red_bubbles);
+  
+  SECTION("Equal levels") {
+    REQUIRE(white_level == identical_white_level);
+  }
+
+  SECTION("Unequal levels") {
+    REQUIRE_FALSE(white_level == red_level);
+  }
+}
+
+TEST_CASE("Test proper level logic") {
+  std::vector<Bubble> white_bubbles;
+  for (size_t i = 0; i < 5; ++i) {
+    Bubble bubble("white", glm::vec2{0,0}, 5.0f,
+                  bubblebounce::Bubble::NormalBubble,
+                  bubblebounce::Bubble::Strong);
+    white_bubbles.emplace_back(bubble);
+  }
+
+  GameLevel white_level(1, 5, white_bubbles);
+
+  SECTION("Level won proper conditions") {
+    std::vector<Bubble> empty_bubbles;
+    GameLevel level(1, 5, empty_bubbles);
+    REQUIRE(level.IsLevelWon());
+  }
+
+  SECTION("Level won is false with wrong conditions") {
+    GameLevel level(1, 5, white_bubbles);
+    REQUIRE_FALSE(level.IsLevelWon());
+  }
+
+  
+  SECTION("Level lost with proper conditions") {
+    GameLevel level(1, 0, white_bubbles);
+    REQUIRE(level.IsLevelLost());
+  }
+
+  SECTION("Level lost is false with wrong condition") {
+    GameLevel level(1, 5, white_bubbles);
+    REQUIRE_FALSE(level.IsLevelLost());
+  }
+  
+  SECTION("Level over") {
+    GameLevel level(1, 0, white_bubbles);
+    REQUIRE(level.IsLevelLost());
+  }
+
+  SECTION("Resetting the level for the round") {
+    white_level.ResetLevelForRound();
+    REQUIRE(white_level.GetPlayerLives() == 4);
   }
 }
